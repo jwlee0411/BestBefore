85108c4c10dbd86793ca9c8fa042d7d04b7b264e
jw <sunrinint@gmail.com>
2020-11-16 20:02:45 +0900

    RecyclerView 수정_1116

--- app/src/main/java/app/sunrin/bestbefore/RecyclerAdapter.java
+++ app/src/main/java/app/sunrin/bestbefore/RecyclerAdapter.java
@@ -2,54 +2,84 @@ package app.sunrin.bestbefore;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder> {
 
-    ArrayList<FoodInformation> foodInformationBundle = new ArrayList<FoodInformation>();
-    Context context;
-    public class ViewHolder extends RecyclerView.ViewHolder{
-        TextView textCategory;
-        TextView textName;
-        TextView textDay;
+    private ArrayList<Data> listData = new ArrayList<>();
 
-        public ViewHolder(@NonNull View v){
-            super(v);
-            this.textCategory = v.findViewById(R.id.textCategory);
-            this.textName = v.findViewById(R.id.textName);
-            this.textDay = v.findViewById(R.id.textDay);
-        }
-    }
-
-    public RecyclerAdapter(ArrayList<FoodInformation> foodInformationBundle){
-        this.foodInformationBundle =foodInformationBundle;
-    }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        return new ViewHolder(inflater.inflate(R.layout.item, parent, false));
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // LayoutInflater를 이용하여 전 단계에서 만들었던 item.xml을 inflate 시킵니다.
+        // return 인자는 ViewHolder 입니다.
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        return new ItemViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        FoodInformation foodInformation = this.foodInformationBundle.get(position);
-        holder.textCategory.setText(foodInformation.category.get(0));
-        holder.textName.setText(foodInformation.productName);
-        holder.textDay.setText(foodInformation.dateLeft);
+    public void onBindViewHolder(@NonNull RecyclerAdapter.ItemViewHolder holder, int position) {
+        // Item을 하나, 하나 보여주는(bind 되는) 함수입니다.
+        holder.onBind(listData.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return this.foodInformationBundle.size();
+        // RecyclerView의 총 개수 입니다.
+        return listData.size();
+    }
+
+    void addItem(Data data) {
+        // 외부에서 item을 추가시킬 함수입니다.
+        listData.add(data);
+    }
+
+
+    class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        ConstraintLayout constraintLayout;
+        TextView textName;
+        TextView textCategory;
+        TextView textDate;
+
+
+        ItemViewHolder(View itemView) {
+            super(itemView);
+
+             constraintLayout = itemView.findViewById(R.id.MainRecyclerItem);
+                textName = itemView.findViewById(R.id.textName);
+                textCategory = itemView.findViewById(R.id.textCategory);
+                textDate = itemView.findViewById(R.id.textDate);
+
+
+
+        }
+
+        void onBind(Data data) {
+
+            textName.setText(data.getProductName());
+            textCategory.setText(data.getProductCategory());
+            textDate.setText(data.getProductDate());
+
+
+
+
+            constraintLayout.setOnClickListener(v ->
+                    System.out.println("Clicked"));
+
+        }
     }
 }
